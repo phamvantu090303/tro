e0249e2c8c08bf89d0b3a10ab0a98550c260c5dc
Hùng <hhuhnhg@gmail.com>
2025-02-12 12:37:46 +0700

    code Get all phong tro

--- BE/src/routers/phongTro.ts
+++ BE/src/routers/phongTro.ts
@@ -12,7 +12,7 @@ const routerPhong = Router();
 
 routerPhong.post("/create", storePhongTro);
 routerPhong.post("/update", updatePhongTro);
-routerPhong.get("/get", getData);
+routerPhong.get("/get/:ma_danh_muc", getData);
 routerPhong.post("/delete/all", deleteAll);
 routerPhong.post("/delete/:ma_phong", deleteById);
 routerPhong.post("/detail/:ma_phong", detailRoom);
