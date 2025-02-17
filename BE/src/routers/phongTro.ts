a00aaea5e81ac14ca6e01329b6a73f11e0d90e0d
Hùng <hhuhnhg@gmail.com>
2025-02-17 17:47:26 +0700

    Cập nhật tính năng mới

--- BE/src/routers/phongTro.ts
+++ BE/src/routers/phongTro.ts
@@ -15,5 +15,6 @@ routerPhong.post("/update/:ma_phong", updatePhongTro);
 routerPhong.get("/get", getData);
 routerPhong.delete("/delete/all", deleteAll);
 routerPhong.delete("/delete/:ma_phong", deleteById);
+
 routerPhong.post("/detail/:ma_phong", detailRoom);
 export default routerPhong;
