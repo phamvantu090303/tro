8c94a59659f5d282dea0e75be70ebb2d66c2185a
Hùng <hhuhnhg@gmail.com>
2025-02-21 11:26:56 +0700

    Add filterAdmin.js

---
+++ FE/src/Store/filterAdmin.js
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  token: localStorage.getItem("token") || null, // Lấy token từ localStorage
+  admin: JSON.parse(localStorage.getItem("admin")) || null,
+};
+
+const authSlice = createSlice({
+  name: "authAdmin",
+  initialState,
+  reducers: {
+    loginAdmin: (state, action) => {
+      state.admin = action.payload.admin;
+      state.token = action.payload.token;
+      state.isAuthenticated = true;
+      localStorage.setItem("token", state.token);
+      localStorage.setItem("admin", JSON.stringify(state.admin));
+    },
+    logoutAdmin: (state) => {
+      state.token = null;
+      state.admin = null;
+      state.isAuthenticated = false;
+      localStorage.removeItem("access_token");
+      localStorage.removeItem("admin");
+    },
+  },
+});
+
+export const { loginAdmin, logoutAdmin } = authSlice.actions;
+
+export default authSlice.reducer;
