e03137609a7e2921476527f02825b2473b497419
tranminhhung28012002 <himden9999@gmail.com>
2025-03-29 20:58:42 +0700

    fix thietb

--- FE/src/pages/RoomDetails/RoomDetails.jsx
+++ FE/src/pages/RoomDetails/RoomDetails.jsx
@@ -17,7 +17,8 @@ import { useSelector } from "react-redux";
 import { usePhongTro } from "../../Context/PhongTroContext";
 import RoomReview from "../../component/RoomDetailsComponent/Review";
 import { toast } from "react-toastify";
-import DeviceSelector from "../../component/Categories/Categories";
+import DeviceSelector from "../../component/thietbi/device";
+
 const slideUpVariants = {
   hidden: { opacity: 0, y: 70 },
   visible: { opacity: 1, y: 0, transition: { duration: 1.2, ease: "easeOut" } },
