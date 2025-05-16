50931303a57f7e037b42e53536b3adcbf4bc74d4
tranminhhung28012002 <himden9999@gmail.com>
2025-05-17 04:25:07 +0700

    fix testcase 1.3

--- FE/src/hook/useMasking.js
+++ FE/src/hook/useMasking.js
@@ -18,11 +18,21 @@ const maskCCCD = (cccd) => {
 };
 
 const formatDate = (dateString) => {
+  if (!dateString) return "";
   const date = new Date(dateString);
   const day = String(date.getDate()).padStart(2, "0");
   const month = String(date.getMonth() + 1).padStart(2, "0");
   const year = date.getFullYear();
   return `${day}/${month}/${year}`;
+};
+
+const formatDateInput = (dateString) => {
+  if (!dateString) return "";
+  const date = new Date(dateString);
+  const day = String(date.getDate()).padStart(2, "0");
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const year = date.getFullYear();
+  return `${year}-${month}-${day}`;
 };
 
 export const formatTienPhong = (number) => {
@@ -36,6 +46,7 @@ export const useMasking = () => {
     maskPhone,
     maskCCCD,
     formatDate,
+    formatDateInput,
     formatTienPhong,
   };
 };
